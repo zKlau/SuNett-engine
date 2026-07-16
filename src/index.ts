8ffe58e242675e301a3d27a9996c1040d4a0a093
zKlau <claudiupadure50@yahoo.com>
2026-07-16 12:38:53 +0000

    Add public package entry

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello, world!");
+export { TabsRenderer } from "./utils/tabs/tabsRenderer";
+export { SongHelper } from "./utils/songHelper";
+
+export type { TabRendererOptions } from "./types/UI/rendererOptions";
+export type { TabNoteOptions } from "./types/UI/tabNoteOptions";
+export type { NoteRenderContext } from "./types/UI/noteRenderContext";
+
+export type { Song } from "./types/song";
+export type { Track } from "./types/track";
+export type { Measure, MeasureHeader } from "./types/measure";
+export type { Voice } from "./types/voice";
+export type { Beat } from "./types/beats/beat";
+export type { Note } from "./types/note";
